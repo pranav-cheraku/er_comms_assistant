68ab28b6077c8515e37b0b6f0e223c8ca1f3f3cd
Jeffrey-F-Guo <jeffrey.guo00703@gmail.com>
2025-06-10 22:29:09 -0700

    Delete backend/transformer directory

--- backend/transformer/attention.py
+++
@@ -1,8 +0,0 @@
-import torch
-import torch.nn as nn
-class MultiHeadedAttention(nn.module):
-    def __init__(embed_size, downsample_size):
-        super.__init__()
-        self.w_q = nn.Linear(embed_size, downsample_size, bias=False)
-        self.w_k = nn.Linear(embed_size, downsample_size, bias=False)
-        self.w_v = nn.Linear(embed_size, downsample_size, bias=False)
